321a29f42a09a42fc2f993ef90cdbdaf5d1b40ac
Jaqueline de Almeida <jaqueshanti@gmail.com>
2024-08-18 16:18:57 -0300

    refactor: adds view to file attached and changes document controller

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -8,6 +8,7 @@ class DocumentsController < ApplicationController
 
   # GET /documents/1 or /documents/1.json
   def show
+    @document = Document.find(params[:id])
   end
 
   # GET /documents/new
@@ -23,14 +24,11 @@ class DocumentsController < ApplicationController
   def create
     @document = Document.new(document_params)
 
-    respond_to do |format|
-      if @document.save
-        format.html { redirect_to document_url(@document), notice: "Document was successfully created." }
-        format.json { render :show, status: :created, location: @document }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @document.errors, status: :unprocessable_entity }
-      end
+    if @document.save
+      ProcessDocumentJob.perform_later(@document.id)
+      redirect_to @document, notice: 'Document sends with successfull'
+    else
+      render :new
     end
   end
 
@@ -38,7 +36,7 @@ class DocumentsController < ApplicationController
   def update
     respond_to do |format|
       if @document.update(document_params)
-        format.html { redirect_to document_url(@document), notice: "Document was successfully updated." }
+        format.html { redirect_to document_url(@document), notice: "Document updates with successfully." }
         format.json { render :show, status: :ok, location: @document }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,13 +56,13 @@ class DocumentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_document
       @document = Document.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to documents_path, alert: "Document not found"
     end
 
-    # Only allow a list of trusted parameters through.
     def document_params
-      params.require(:document).permit(:user_id, :file, :description)
+      params.require(:document).permit(:file_name, :description, :file)
     end
 end
