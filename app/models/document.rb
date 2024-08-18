d9551785ff37e4352180e211500e549de3acedac
Jaqueline de Almeida <jaqueshanti@gmail.com>
2024-08-18 00:36:34 -0300

    fix: allows file upload and shows link and content

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,8 +1,23 @@
 class Document < ApplicationRecord
-  belongs_to :user
+  #belongs_to :user
 
   has_one_attached :file
   # has_many_attached :files
 
-  validates :file, attached: true, content_type: ['application/xml']
+  validates :file, attached: true
+
+  validate :correct_file_type
+
+  private
+
+  def correct_file_type
+    if file.attached?
+      allowed_types = ['text/xml', 'application/xml', 'application/x-xml']
+      unless allowed_types.include?(file.content_type)
+        errors.add(:file, 'Must be an XML file')
+      end
+    else
+      errors.add(:file, 'Must be attached')
+    end
+  end
 end
